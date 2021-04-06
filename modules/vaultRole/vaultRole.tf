3d0ee4bbf24d62e8d327223c5294ce469379256a
Tadeos <tadeos.melaku@sap.com>
2021-04-06 06:41:44 -0700

    .

--- modules/vaultRole/vaultRole.tf
+++ modules/vaultRole/vaultRole.tf
@@ -8,15 +8,13 @@ resource "vault_aws_secret_backend" "aws" {
 
 data "template_file" "policy" {
     template = file("${path.module}/iamPolicies.json")
-    #template = file("./iamPolicies.json")
 }
 
 resource "vault_aws_secret_backend_role" "roleSet" {
     backend         = vault_aws_secret_backend.aws.path
     credential_type = "assumed_role"
-    name = "AWS-vault-full-role"
+    name = "AWS-full-role"
     policy_document = data.template_file.policy.rendered
-    #policy_document = var.policy_doc
     role_arns      = [var.rolearn["${var.service}-sap-ariba-${var.env}-${var.dc}-${var.acct_type}-bootstrap"]]
     depends_on = [vault_aws_secret_backend.aws]
 }
